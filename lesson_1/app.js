03533b008f878666e250e975d8cf37ae77110823
elmar <leokorolev2@gmail.com>
2024-11-14 21:19:32 +0600

    add lesson 2

---
+++ lesson_1/app.js
@@ -0,0 +1,8 @@
+        console.log ("Hello world!");
+        alert("Hello JavaScript");
+        document.write("Hello Group 44");
+
+
+        document.body.style.color = "white";
+        document.body.style.fontSize = "6rem";
+        document.body.style.fontFamily = "Arial, Helvetice, sans-serif";
