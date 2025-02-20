d9a97fbc08446f4103ef8ddf3d455d07b2d62320
elmar <leokorolev2@gmail.com>
2025-02-20 19:17:27 +0600

    Delete lesson_1 directory

--- lesson_1/app.js
+++
@@ -1,8 +0,0 @@
-        console.log ("Hello world!");
-        alert("Hello JavaScript");
-        document.write("Hello Group 44");
-
-
-        document.body.style.color = "white";
-        document.body.style.fontSize = "6rem";
-        document.body.style.fontFamily = "Arial, Helvetice, sans-serif";
