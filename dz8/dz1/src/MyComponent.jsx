0749646e0987e6655883ff4bd127d5d71fa599a9
elmar <leokorolev2@gmail.com>
2025-03-01 11:54:39 +0600

    Add files via upload

---
+++ dz8/dz1/src/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div className="my-component">
+      <p>Это мой компонент, который я переиспользую!</p>
+    </div>
+  );
+};
+
+export default MyComponent;
+
+
